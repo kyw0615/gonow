77e4450be5a5c985bd73d41ab3541fa685f2887b
kyw0615 <kimyo0615@naver.com>
2021-10-14 14:15:49 +0900

    Update MainActivity.java

--- app/src/main/java/com/capstone/gonow/MainActivity.java
+++ app/src/main/java/com/capstone/gonow/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //changes no no!! asdf
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
