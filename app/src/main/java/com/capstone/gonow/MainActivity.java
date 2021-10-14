60f94d2b03219208efffe46a8253f91cc665d557
dltngud1541 <58357582+dltngud1541@users.noreply.github.com>
2021-10-14 15:00:02 +0900

    Update MainActivity.java
    Co-Authored-By: jewjd0 <86175802+jewjd0@users.noreply.github.com>
    Co-Authored-By: kyw0615 <81142553+kyw0615@users.noreply.github.com>

--- app/src/main/java/com/capstone/gonow/MainActivity.java
+++ app/src/main/java/com/capstone/gonow/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //change
+    //change 1234
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
