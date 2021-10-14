7023c21a29520b897c45de2f0fb0e6d20cc8d398
kyw0615 <kimyo0615@naver.com>
2021-10-14 14:07:23 +0900

    1

--- app/src/main/java/com/capstone/gonow/MainActivity.java
+++ app/src/main/java/com/capstone/gonow/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //changes no
+    //changes no no!!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
